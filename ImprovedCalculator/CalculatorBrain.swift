6a57c9bd864f81ac1146016b18ca6386cf923179
M <lmiguelvargasf@gmail.com>
2016-09-24 17:44:37 -0500

    Initial commit

---
+++ ImprovedCalculator/CalculatorBrain.swift
@@ -0,0 +1,140 @@
+import Foundation
+
+func factorial(number: Double) -> Double {
+    if number == 0.0 || number == 1.0 {
+        return 1.0
+    }
+    return factorial(number: number - 1.0)
+}
+
+class CalculatorBrain {
+    
+    private var accumulator = 0.0
+    
+    private var wasPreviousConstant = false
+    
+    var result: Double {
+        return accumulator
+    }
+    var description = ""
+    
+    var isPartialResult = false
+    
+    func setOperand(operand: Double) {
+        if !isPartialResult {
+            description = ""
+        }
+        accumulator = operand
+        isPartialResult = true
+    }
+    
+    private enum Operator {
+        case Constant(Double)
+        case Unary((Double) -> Double)
+        case Binary((Double, Double) -> Double)
+        case Equal
+        case Random
+    }
+    
+    private let operations = [
+        "π": Operator.Constant(M_PI),
+        "e": Operator.Constant(M_E),
+        "±": Operator.Unary({ -1 * $0 }),
+        "√": Operator.Unary(sqrt),
+        "∛": Operator.Unary({ pow($0, 1.0 / 3.0) }),
+        "x⁻¹": Operator.Unary({ 1 / $0 }),
+        "x²": Operator.Unary({ $0 * $0}),
+        "x³": Operator.Unary({ pow($0, 3) }),
+        "10^x": Operator.Unary({ pow(10, $0) }),
+        "e^x": Operator.Unary({ pow(M_E, $0) }),
+        "ln": Operator.Unary(log),
+        "log": Operator.Unary(log10),
+        "sin": Operator.Unary(sin),
+        "cos": Operator.Unary(cos),
+        "tan": Operator.Unary(tan),
+        "sin⁻¹": Operator.Unary(asin),
+        "cos⁻¹": Operator.Unary(acos),
+        "tan⁻¹": Operator.Unary(atan),
+        "x!": Operator.Unary(factorial),
+        "×": Operator.Binary(*),
+        "÷": Operator.Binary(/),
+        "+": Operator.Binary(+),
+        "−": Operator.Binary(-),
+        "x^y": Operator.Binary({ pow($0, $1) }),
+        "=": Operator.Equal,
+        "rnd": Operator.Random
+        ]
+    
+    private struct PendingBinaryOperation {
+        let binaryFunction: (Double, Double) -> Double
+        let fistOperand: Double
+    }
+    
+    private var pending: PendingBinaryOperation?
+    
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let constant):
+                if wasPreviousConstant {
+                    description = ""
+                }
+                
+                description += symbol
+                accumulator = constant
+                isPartialResult = false
+                wasPreviousConstant = true
+                return
+            case .Unary(let function):
+                if isPartialResult {
+                    description += symbol + "(" + String(accumulator) + ")"
+                    isPartialResult = false
+                } else {
+                    if description == "" {
+                        description = symbol + "(0)"
+                    } else {
+                        description = symbol + "(" + description + ")"
+                    }
+                }
+                accumulator = function(accumulator)
+            case .Binary(let function):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperation(binaryFunction: function, fistOperand: accumulator)
+                if isPartialResult {
+                    switch accumulator {
+                    case M_PI:
+                        description += "π" + symbol
+                    case M_E:
+                        description += "e" + symbol
+                    default:
+                        description += String(accumulator) + symbol
+                    }
+                } else {
+                    if description == "" {
+                        description = "0" + symbol
+                    } else {
+                        description += symbol
+                    }
+                }
+                isPartialResult = true
+            case .Equal:
+                executePendingBinaryOperation()
+            case .Random:
+                accumulator = drand48()
+                description += String(accumulator)
+            }
+            wasPreviousConstant = false
+        }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            if isPartialResult {
+                description += String(accumulator)
+            }
+            accumulator = pending!.binaryFunction(pending!.fistOperand, accumulator)
+            pending = nil
+            isPartialResult = false
+        }
+    }
+}
---
+++ ImprovedCalculator/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  ImprovedCalculator
+//
+//  Created by user on 9/23/16.
+//  Copyright © 2016 mathsistor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var isShiftPressed = false
+    private var isUserInMiddleOfTyping = false
+    private var brain = CalculatorBrain()
+    @IBOutlet weak var expressionDescription: UILabel!
+    
+    @IBOutlet private weak var sin: UIButton!
+    @IBOutlet private weak var cos: UIButton!
+    @IBOutlet private weak var tan: UIButton!
+    @IBOutlet weak var square: UIButton!
+    @IBOutlet weak var cube: UIButton!
+    
+    
+    private var displayedValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+
+        set {
+            display.text = String(newValue)
+            expressionDescription.text = brain.description + ( brain.isPartialResult ? "..." : "=")
+            
+        }
+    }
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    
+    @IBAction private func touchButton(_ sender: UIButton) {
+        let character = sender.currentTitle!
+        
+        if isUserInMiddleOfTyping {
+            guard character != "." || !isPointInDisplayedData() else {
+                return
+            }
+            
+            let currentDisplayedData = display.text!
+            display.text = currentDisplayedData + character
+        } else {
+            if character == "." {
+                display.text = "0" + character
+            } else {
+                display.text = character
+            }
+        }
+        
+        isUserInMiddleOfTyping = true
+    }
+    
+    private func isPointInDisplayedData() -> Bool {
+        return display.text!.range(of: ".") != nil
+    }
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        let symbol = sender.currentTitle!
+        
+        if isUserInMiddleOfTyping {
+            brain.setOperand(operand: displayedValue)
+            isUserInMiddleOfTyping = false
+        }
+        
+        brain.performOperation(symbol: symbol)
+        displayedValue = brain.result
+        
+    }
+    
+    @IBAction private func clearEverything(_ sender: AnyObject) {
+        display.text = "0"
+        expressionDescription.text = "0"
+        brain = CalculatorBrain()
+        isUserInMiddleOfTyping = false
+    }
+    
+    @IBAction func shift(_ sender: UIButton) {
+        isShiftPressed = !isShiftPressed
+        if isShiftPressed {
+            sender.backgroundColor = UIColor(red: CGFloat(175.0 / 255.0), green: CGFloat(128.0 / 255.0), blue: CGFloat(229.0 / 255.0), alpha: 1)
+            sin.setTitle("sin⁻¹", for: .normal)
+            cos.setTitle("cos⁻¹", for: .normal)
+            tan.setTitle("tan⁻¹", for: .normal)
+            square.setTitle("x²", for: .normal)
+            cube.setTitle("x³", for: .normal)
+        } else {
+            sender.backgroundColor = UIColor(red: CGFloat(127.0 / 255.0), green: CGFloat(127.0 / 255.0), blue: CGFloat(127.0 / 255.0), alpha: 1)
+            sin.setTitle("sin", for: .normal)
+            cos.setTitle("cos", for: .normal)
+            tan.setTitle("tan", for: .normal)
+            square.setTitle("√", for: .normal)
+            cube.setTitle("∛", for: .normal)
+        }
+    }
+
+}
+
