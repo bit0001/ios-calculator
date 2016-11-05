34f8994698facf27103b0d0ca664b16113788f08
M <lmiguelvargasf@gmail.com>
2016-11-05 16:45:59 -0500

    test: Implement remaining provided cases

--- ImprovedCalculatorUITests/DescriptionUITests.swift
+++ ImprovedCalculatorUITests/DescriptionUITests.swift
@@ -65,4 +65,98 @@ class DescriptionUITests: XCTestCase {
         app.buttons["√"].tap()
     }
     
+    func test7Plus9SquareRoot() {
+        expectedResultDisplay = "3"
+        expectedOperationDisplay = "7+√(9)..."
+        
+        app.buttons["7"].tap()
+        app.buttons["+"].tap()
+        app.buttons["9"].tap()
+        app.buttons["√"].tap()
+    }
+    
+    func test7Plus9SquareRootEqual() {
+        expectedResultDisplay = "10"
+        expectedOperationDisplay = "7+√(9)="
+        
+        app.buttons["7"].tap()
+        app.buttons["+"].tap()
+        app.buttons["9"].tap()
+        app.buttons["√"].tap()
+        app.buttons["="].tap()
+    }
+    
+    func test7Plus9EqualPlus6Plus3Equal() {
+        expectedResultDisplay = "25"
+        expectedOperationDisplay = "7+9+6+3="
+        
+        app.buttons["7"].tap()
+        app.buttons["+"].tap()
+        app.buttons["9"].tap()
+        app.buttons["="].tap()
+        app.buttons["+"].tap()
+        app.buttons["6"].tap()
+        app.buttons["+"].tap()
+        app.buttons["3"].tap()
+        app.buttons["="].tap()
+    }
+    
+    func test7Plus9EqualSquareRoot6Plus3Equal() {
+        expectedResultDisplay = "9"
+        expectedOperationDisplay = "6+3="
+        
+        app.buttons["7"].tap()
+        app.buttons["+"].tap()
+        app.buttons["9"].tap()
+        app.buttons["="].tap()
+        app.buttons["√"].tap()
+        app.buttons["6"].tap()
+        app.buttons["+"].tap()
+        app.buttons["3"].tap()
+        app.buttons["="].tap()
+    }
+    
+    func test5Plus6Equal73() {
+        expectedResultDisplay = "73"
+        expectedOperationDisplay = "5+6="
+        
+        app.buttons["5"].tap()
+        app.buttons["+"].tap()
+        app.buttons["6"].tap()
+        app.buttons["="].tap()
+        app.buttons["7"].tap()
+        app.buttons["3"].tap()
+    }
+    
+    func test7PlusEqual() {
+        expectedResultDisplay = "14"
+        expectedOperationDisplay = "7+7="
+        
+        app.buttons["7"].tap()
+        app.buttons["+"].tap()
+        app.buttons["="].tap()
+    }
+    
+    func test4TimesPiEqual() {
+        expectedResultDisplay = "12.566371"
+        expectedOperationDisplay = "4×π="
+        
+        app.buttons["4"].tap()
+        app.buttons["×"].tap()
+        app.buttons["π"].tap()
+        app.buttons["="].tap()
+    }
+    
+    func test4Plus5Times3Equal() {
+        expectedResultDisplay = "27"
+        expectedOperationDisplay = "4+5×3="
+        
+        app.buttons["4"].tap()
+        app.buttons["+"].tap()
+        app.buttons["5"].tap()
+        app.buttons["×"].tap()
+        app.buttons["3"].tap()
+        app.buttons["="].tap()
+    }
+    
 }
